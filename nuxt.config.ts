bfb1912a8f11e6d85b9f164b485b18edcdda76e1
grothesk <malte.groth@posteo.net>
2023-11-20 00:47:56 +0100

    Init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  app: {
+    baseURL: "/blog/"
+  },
+
+  // https://github.com/nuxt-themes/alpine
+  extends: '@nuxt-themes/alpine',
+
+  modules: [
+    // https://github.com/nuxt-modules/plausible
+    '@nuxtjs/plausible',
+    // https://github.com/nuxt/devtools
+    '@nuxt/devtools'
+  ]
+})
